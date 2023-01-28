62a8115e6bcc78986760ff38971594f6cac4e61f
Dhawkins777 <dhawkins314@gmail.com>
2023-01-27 19:27:21 -0500

    stuff

--- digi-react/src/pages/home/index.js
+++ digi-react/src/pages/home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import style from './style.module.scss';
 
 const HomePage = () => {
-
+    
+    <div className={style.HomePage}>
+        
+    </div>
     return (
         <div>
             <h2>This is the home Page</h2>
         </div>
     );
+
+
 }
 
 export default HomePage;
