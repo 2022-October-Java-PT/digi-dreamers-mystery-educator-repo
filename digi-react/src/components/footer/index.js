32591c1f8574835473301173f9d995f26da4e306
conniecon <littlemissconnie@yahoo.com>
2023-01-28 21:23:55 -0500

    more fun with commits

--- digi-react/src/components/footer/index.js
+++ digi-react/src/components/footer/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import style from './style.module.scss';
-
 const Footer = () => (
   <div className={style.footer}>
     <small>&copy; Digi-Dreamers 2023</small>
     <ul className={style.socialList}>
       <li>GitHub</li>
-      
     </ul>
   </div>
 );
-
 export default Footer;
