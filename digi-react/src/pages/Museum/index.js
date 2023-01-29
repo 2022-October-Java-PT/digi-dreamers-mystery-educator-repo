617a9e5392914f351547fbd89accc8526b5f1164
conniecon <littlemissconnie@yahoo.com>
2023-01-28 20:03:16 -0500

    added to museum page

--- digi-react/src/pages/Museum/index.js
+++ digi-react/src/pages/Museum/index.js
@@ -10,11 +10,9 @@ const MetMuseumPage = () => {
   useEffect(() => {
 
     const fetchMetMuseumData = async () => {
-        const result = await Axios('https://collectionapi.metmuseum.org/public/collection/v1/objects/436965');
-        // const result2 = await Axios('https://collectionapi.metmuseum.org/public/collection/v1/objects/438815');
+        const result = await Axios('https://collectionapi.metmuseum.org/public/collection/v1/objects/436532');
         
         setMetMuseum(result.data);
-        // setMetMuseum(result2.data);
     }
 
     if(metMuseum) {
@@ -26,25 +24,28 @@ const MetMuseumPage = () => {
     }, 1000);
 
     return () => clearTimeout(timer);
-
-    // fetchMetMuseum();
       
   }, [metMuseum]);
   
-  const met_key = '436965';
-  // const met_key2 = '438815';
+  const met_key = '436532';
 
   return (
     <div className='style'>
       <h2>The Metropolitan Museum of Art</h2>
-      <h2>Art Picture of the Day</h2>
+      <h2>Artwork of the Day</h2>
       {loading ? <h3>Loading...</h3> : [metMuseum].map(metMuseum => (
-        <>
-        <a key={met_key} href={`metMuseum/${metMuseum.id}`}>
-        <img src={metMuseum.primaryImage} width='400' height='auto' /></a>
-        {/* <a key={met_key2} href={`metMuseum/${metMuseum.id}`}>
-        <img src={metMuseum.primaryImage} width='200' height='auto' /></a> */}
-        </>
+        
+        <p key={met_key} href={`metMuseum/${metMuseum.id}`}>
+        <h3>Artist</h3>
+        <p>{metMuseum.artistDisplayName}</p>
+        <h3>Artist Bio</h3>
+        <p>{metMuseum.artistDisplayBio}</p>
+        <h3>Title</h3>
+        <p>{metMuseum.title}</p>
+        <h3>Medium</h3>
+        <p>{metMuseum.medium}</p>
+        <img src={metMuseum.primaryImage} alt="" width='900' height='auto' />
+        </p>
       ))}
     
     </div>
