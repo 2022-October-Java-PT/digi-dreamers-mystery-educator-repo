c64768b77d8d1ce87e7c5f2408c462a85b318bcc
Dhawkins777 <dhawkins314@gmail.com>
2023-01-30 13:25:47 -0500

    update

--- digi-react/src/components/header/index.js
+++ digi-react/src/components/header/index.js
@@ -11,6 +11,7 @@ const Header = () => (
       <NavLink to='/Museum'>MetMuseum</NavLink>
       <NavLink to='/Funfact'>FunFact</NavLink>
       <NavLink to='/Contact'>Contact</NavLink>
+      <NavLink to='/Games'>Games</NavLink>
     </ul>
   </div>
 );
---
+++ digi-react/src/pages/games/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GamesPage = () => {
+    
+    <div className={style.GamesPage}>
+        <div>
+
+            
+        </div>
+
+    </div>
+
+}
