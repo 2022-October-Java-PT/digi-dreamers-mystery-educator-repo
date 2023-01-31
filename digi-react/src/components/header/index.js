2a3f19f6680c3f3007979c8576581b2b0ec0007b
conniecon <littlemissconnie@yahoo.com>
2023-01-30 21:20:50 -0500

    about styling

--- digi-react/src/components/header/index.js
+++ digi-react/src/components/header/index.js
@@ -10,6 +10,7 @@ const Header = () => (
       <NavLink to='/Nasa'>Nasa</NavLink>
       <NavLink to='/Museum'>MetMuseum</NavLink>
       <NavLink to='/Funfact'>FunFact</NavLink>
+      <NavLink to='/about'>About</NavLink>
     </ul>
   </div>
 );
