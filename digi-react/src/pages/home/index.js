f905801589450f1ebad2be4af39e7113ff09cebf
Dhawkins777 <dhawkins314@gmail.com>
2023-02-01 17:27:14 -0500

    paragraph on MET and game unable to work

--- digi-react/src/pages/home/index.js
+++ digi-react/src/pages/home/index.js
@@ -39,6 +39,9 @@ const HomePage = () => {
         </center>
         <center>
         <h2>Take a virtual tour of the MET!</h2>
+        <p>
+        The Metropolitan Museum of Art presents over 5,000 years of art from around the world for everyone to experience and enjoy. The Museum lives in two iconic sites in New York City—The Met Fifth Avenue and The Met Cloisters. Millions of people also take part in The Met experience online. Since its founding in 1870, The Met has always aspired to be more than a treasury of rare and beautiful objects. Every day, art comes alive in the Museum's galleries and through its exhibitions and events, revealing new ideas and unexpected connections across time and across cultures.
+        </p>
         </center>
         <center>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/wgpOsxHkeTw?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
