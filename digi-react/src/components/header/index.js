8c67193bebff32026b37e2516ac677eb6e91e92d
conniecon <littlemissconnie@yahoo.com>
2023-02-01 08:18:09 -0500

    header/footer styles

--- digi-react/src/components/header/index.js
+++ digi-react/src/components/header/index.js
@@ -8,9 +8,9 @@ const Header = () => (
     <ul className={style.navList}>
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/Nasa'>Nasa</NavLink>
-      <NavLink to='/NasaGallery'>NasaGallery</NavLink>
-      <NavLink to='/Museum'>MetMuseum</NavLink>
-      <NavLink to='/Funfact'>FunFact</NavLink>
+      <NavLink to='/NasaGallery'>Nasa Gallery</NavLink>
+      <NavLink to='/Museum'>Met Museum</NavLink>
+      <NavLink to='/Funfact'>Fun Fact</NavLink>
       <NavLink to='/about'>About</NavLink>
       <NavLink to='/Games'>Games</NavLink>
       <NavLink to='/Contact'>Contact</NavLink>
